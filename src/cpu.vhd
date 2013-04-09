ae7e3e0be3b2da423add0e93249a6bb99d7bcc93
Alex Telon <alex.telon@gmail.com>
2013-04-09 12:41:27 +0200

    added testbench for cpu

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity Cpu is
   port(Clk, Reset : in std_logic;
@@ -16,13 +17,51 @@ architecture Cpu_Implementation of Cpu is
   signal SR : std_logic_vector(7 downto 0);
   -- Stack pointer and program counter
   signal SP, PC : std_logic_vector(15 downto 0);
+
+  type State_Type is (Waiting, Fetch, Exec);
+  -- current state of the interpreter
+  signal State : State_Type := Waiting;
+  -- how long have we been waiting?
+  signal Waited_Clks : std_logic_vector(15 downto 0) := X"0000";
   
   
 begin
   -- 
   process(Clk)
   begin
-
+    if rising_edge(Clk) then
+      if (Reset = '1') then
+        Waited_Clks <= X"0000";
+        State <= Waiting;
+        PC <= X"0000"; -- TODO Not the real default
+        -- Reset SP?
+        A <= X"01";
+        B <= X"00";
+      else
+        
+        Waited_Clks <= std_logic_vector(unsigned(Waited_Clks) + 1);
+        case (State) is
+          when Waiting =>
+            if (unsigned(Waited_Clks) > 5) then
+              State <= Fetch;
+              Waited_Clks <= X"0000";
+            end if;
+          when Fetch =>
+            Mem_Addr <= PC;
+            State <= Exec;
+          when Exec =>
+            case (Mem_Read) is
+              -- LD A, B
+              when X"78" =>
+                B <= A;
+              when others =>
+                --FAKKA UR TOTALT OCH DÖ.
+            end case;
+            State <= Waiting;
+          when others =>
+        end case;
+      end if;
+    end if;
   end process;
     
 
---
+++ src/cpu_testbench.vhd
@@ -0,0 +1,55 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Cpu_Test is
+end Cpu_Test;
+
+architecture Behavior of Cpu_Test is
+-- Component Decalaration
+  component Cpu
+  port(Clk, Reset : in std_logic;
+       Mem_Write : out std_logic_vector(7 downto 0);
+       Mem_Read : in std_logic_vector(7 downto 0);
+       Mem_Addr : out std_logic_vector(15 downto 0);
+       Mem_Write_Enable : out std_logic);
+  end component;
+  signal Clk, Reset : std_logic;
+  signal Mem_Write : std_logic_vector(7 downto 0);
+  signal Mem_Read : std_logic_vector(7 downto 0);
+  signal Mem_Addr : std_logic_vector(15 downto 0);
+  signal Mem_Write_Enable : std_logic;
+  
+begin
+-- compnent instantiation
+  Cpu_Ports : Cpu port map(
+    Clk => Clk,
+    Reset => Reset,
+    Mem_Write => Mem_Write,
+    Mem_Read => Mem_Read,
+    Mem_Addr => Mem_Addr,
+    Mem_Write_Enable => Mem_Write_Enable);
+
+  Clk_Gen : process
+  begin
+    while (true) loop
+      Clk <= '0';
+      wait for 5 ns;
+      Clk <= '1';
+      wait for 5 ns;
+    end loop;
+  end process;
+
+  Stimuli_Generator : process
+
+  begin
+    Reset <= '1';
+    wait for 500 ns;
+
+    wait until rising_edge(Clk);
+    Reset <= '0';
+
+    Mem_Read <= X"78";
+    wait;      
+  end process;
+  
+end Behavior;
