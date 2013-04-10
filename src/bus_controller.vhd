4f18f88486f6280f69799fc97b7243a8b639477e
Filip Stromback <fstromback@gmail.com>
2013-04-10 13:34:58 +0200

    Updated the bus controller to support writing data to the rom

--- src/bus_controller.vhd
+++ src/bus_controller.vhd
@@ -13,7 +13,14 @@ entity Bus_Controller is
         Mem_Write : in std_logic_vector(7 downto 0);
         Mem_Read : out std_logic_vector(7 downto 0);
         Mem_Addr : in std_logic_vector(15 downto 0);
-        Mem_Write_Enable : in std_logic);
+        Mem_Write_Enable : in std_logic;
+        -- These three signals writes to the large rom.
+        -- Note: The addresses 0x0..0x14F can be written to here
+        -- but they cannot be read back later. This should be fixed
+        -- in the "real" implementation of the rom.
+        Rom_Write_Enable : in std_logic;
+        Rom_Addr : in std_logic_vector(15 downto 0);
+        Rom_Write : in std_logic_vector(7 downto 0));
 end Bus_Controller;
 
 
@@ -24,7 +31,21 @@ architecture Bus_Controller_Behaviour of Bus_Controller is
   signal External_Ram : Ram_8KType;
   signal Internal_Ram : Ram_8KType;
   signal Stack_Ram : Ram_127Type;
+
+  -- TODO: Replace this with the larger external memory!
+  signal Rom_Memory : Ram_8KType;
 begin
+
+  -- Writing to the rom
+  process (Clk)
+  begin
+    if rising_edge(Clk) then
+      if Rom_Write_Enable = '1' then
+        Rom_Memory(to_integer(unsigned(Rom_Addr))) <= Rom_Write;
+      end if;
+    end if;
+  end process;
+
   process (Clk)
   begin
     if rising_edge(Clk) then
@@ -39,8 +60,9 @@ begin
         Mem_Read <= X"00";
       elsif Mem_Addr < X"8000" then
         -- User program area. The ROM inserted into the unit.
-        -- Not implemented yet.
-        Mem_Read <= X"00";
+        -- This implementation only supports the first 8K of the user program area.
+        -- Replace with better implementation later.
+        Mem_Read <= Rom_Memory(to_integer(unsigned(Mem_Addr(12 downto 0))));
       elsif Mem_Addr < X"9800" then
         -- Character data. Send to GPU.
         Mem_Read <= X"00";
