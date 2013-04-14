b509fcbdb06863bf6e4ece958448255e7e1b0ffc
Filip Stromback <fstromback@gmail.com>
2013-04-14 12:46:57 +0200

    Fixed even more of the bus controller implementation. Now the ROM fills up the entire address space from 0x0000 to 0x7FFF, as it is supposed to do.

--- src/bus_controller.vhd
+++ src/bus_controller.vhd
@@ -15,9 +15,6 @@ entity Bus_Controller is
         Mem_Addr : in std_logic_vector(15 downto 0);
         Mem_Write_Enable : in std_logic;
         -- These three signals writes to the large rom.
-        -- Note: The addresses 0x0000..0x00FF can be written to here
-        -- but they cannot be read back later. This should be fixed
-        -- in the "real" implementation of the rom.
         Rom_Write_Enable : in std_logic;
         Rom_Addr : in std_logic_vector(15 downto 0);
         Rom_Write : in std_logic_vector(7 downto 0));
@@ -49,19 +46,16 @@ begin
   process (Clk)
   begin
     if rising_edge(Clk) then
-      if Mem_Addr < X"0100" then
-        -- Interrupt address
-        -- RST address
-        -- Not implemented yet
-        Mem_Read <= X"00";
-      elsif Mem_Addr < X"8000" then
+      if Mem_Addr < X"4000" then
+        -- Addresses 0-100 contains interrupt vectors.
         -- User program area. The ROM inserted into the unit.
         -- NOTE: Address X100 is the actual starting address, but
         -- since it is only three bytes before some data required
         -- by the boot-loader, it usually only contains the instruction
         -- JMP 0x150, which is where the predefined things end.
-        -- This implementation only supports the first 8K of the user program area.
-        -- Replace with better implementation later.
+        Mem_Read <= Rom_Memory(to_integer(unsigned(Mem_Addr(12 downto 0))));
+      elsif Mem_Addr < X"8000" then
+        -- The addresses 4000-7FFF is switchable in som ROMS, implement!
         Mem_Read <= Rom_Memory(to_integer(unsigned(Mem_Addr(12 downto 0))));
       elsif Mem_Addr < X"9800" then
         -- Character data. Send to GPU.
