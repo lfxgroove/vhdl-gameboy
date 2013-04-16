3a58d7cefca457602ae8363091f55ecbb808f3ed
Anton <snor@sael.nu>
2013-04-16 15:58:54 +0100

    Have compile exit on errors

--- src/compile.sh
+++ src/compile.sh
@@ -22,11 +22,43 @@ for file in *.vhd
 do
     echo "Compiling $file"
     #DO _NOT_ MOVE --ieee=synopsys to the end of the line as one would expect should work..
-    ghdl -a --ieee=synopsys ${file}
+    ghdl -a --ieee=synopsys ${file} || exit
 done
 
-if [ $# -gt 2 ]
-then
+if [ $# -eq 1 ]; then
+    if [ $1 = "t" ]; then
+	#Test everything
+	for dir in tests/*; do
+	    if [ ${dir} != "tests/bin" ]; then
+		test_name=`echo ${dir} | sed 's/\(\|_\)test\(s\|\)\(\|_\)//g' | sed 's/\///g'`
+		echo "Compiling test: ${test_name}"
+		ghdl -a --ieee=synopsys ${dir}/${test_name}_test.vhd || exit
+		echo "Simulating test: ${test_name^}"
+		    #Error suppression is used, perhaps this should be removed?
+    		    #ghdl --elab-run --ieee=synopsys ${test_name^}_Test --vcd=${test_name^}_Test.vcd --stop-time=${time}
+                    # > /dev/null 2>&1
+		tester/tester -d ${dir}
+	    fi
+	done
+
+    fi
+    exit;
+fi
+
+if [ $# -eq 2 ]; then
+    #Run individual test
+    test_name=${model_name}
+    test_path=${model_name}_test
+    echo "Compiling and running test ${test_name}"
+    if [ -e "tests/${test_path}" ]; then
+	./tester/tester -d ./tests/${test_path}/
+    else
+	echo "The test: ${test_name} doesn't exist"
+	exit
+    fi
+fi
+
+if [ $# -gt 2 ]; then
     case $3 in
 	r | run)
 	    echo "Simulating and showing results in gtkwave"
@@ -38,19 +70,7 @@ then
 	    simulate $model_name $time
 	    ;;
 	t | testa)
-	    echo "Compiling and running tests"
-	    for dir in tests/*; do
-		if [ ${dir} != "tests/bin" ]; then
-		    test_name=`echo ${dir} | sed 's/\(\|_\)test\(s\|\)\(\|_\)//g' | sed 's/\///g'`
-		    echo "Compiling test: ${test_name}"
-		    ghdl -a --ieee=synopsys ${dir}/${test_name}_test.vhd
-		    echo "Simulating test: ${test_name^}"
-		    #Error suppression is used, perhaps this should be removed?
-		    ghdl --elab-run --ieee=synopsys ${test_name^}_Test --vcd=${test_name^}_Test.vcd --stop-time=${time}
-                    # > /dev/null 2>&1
-		    #Todo: Diff the files in some way!
-		fi
-	    done
+	    show_help
 	    ;;
 	*)
 	    show_help
