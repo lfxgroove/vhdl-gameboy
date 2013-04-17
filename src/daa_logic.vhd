0590edcb0c6348c36810f04eeec975ea14bfec57
Filip Stromback <fstromback@gmail.com>
2013-04-17 10:36:50 +0200

    Fixed the logic of the DAA instruction.

--- src/daa_logic.vhd
+++ src/daa_logic.vhd
@@ -43,14 +43,49 @@ end Daa_Logic;
 architecture Daa_Implementation of Daa_Logic is
   signal C, H, C_Out, Z_Out : std_logic;
 
+  -- Upper and lower 4 bits of the input.
+  signal Upper, Lower : std_logic_vector(3 downto 0);
+
+  -- The value to add to Input
   signal To_Add : std_logic_vector(7 downto 0);
+
+  -- Concatenation of the To_Add and C_Out for convenience in the table.
+  signal To_Add_C_Out : std_logic_vector(8 downto 0);
+
+  -- Temorary output (to be able to generate the zero flag).
+  signal Tmp_Output : std_logic_vector(7 downto 0);
 begin
   C <= Flags(4);
   H <= Flags(5);
 
-  Z_Out <= '0';
-  C_Out <= '0';
-  Output <= X"00";
+  Upper <= Input(7 downto 4);
+  Lower <= Input(3 downto 0);
+
+  -- Lookup table according to the above table.
+  To_Add_C_Out <=
+    X"00" & "0" when C = '0' and Upper < X"A" and H = '0' and Lower < X"A" else
+    X"06" & "0" when C = '0' and Upper < X"9" and H = '0' and Lower > X"9" else    
+    X"06" & "0" when C = '0' and Upper < X"A" and H = '1' and Lower < X"4" else
+    X"60" & "1" when C = '0' and Upper > X"9" and H = '0' and Lower < X"A" else
+    X"66" & "1" when C = '0' and Upper > X"8" and H = '0' and Lower > X"9" else
+    X"66" & "1" when C = '0' and Upper > X"9" and H = '1' and Lower < X"4" else
+    X"60" & "1" when C = '1' and Upper < X"3" and H = '0' and Lower < X"A" else
+    X"66" & "1" when C = '1' and Upper < X"3" and H = '0' and Lower > X"9" else
+    X"66" & "1" when C = '1' and Upper < X"4" and H = '1' and Lower < X"4" else
+    X"00" & "0" when C = '0' and Upper < X"A" and H = '0' and Lower < X"A" else
+    X"FA" & "0" when C = '0' and Upper < X"9" and H = '1' and Lower > X"5" else
+    X"A0" & "1" when C = '1' and Upper > X"6" and H = '0' and Lower < X"A" else
+    X"9A" & "1" when C = '1' and Upper > X"5" and H = '1' and Lower > X"5" else
+    X"00" & "0";
+
+  To_Add <= To_Add_C_Out(8 downto 1);
+  C_Out <= To_Add_C_Out(0);
+
+  -- Calculate the result.
+  Tmp_Output <= std_logic_vector(unsigned(Input) + unsigned(To_Add));
+  Z_Out <= '1' when Tmp_Output = X"00" else
+           '0';
+  Output <= Tmp_Output;
 
   Flags_Out(7) <= Z_Out;
   Flags_Out(6) <= Flags(6);
