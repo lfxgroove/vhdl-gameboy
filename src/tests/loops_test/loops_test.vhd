dc226a041f033421be1b3777ebc1d8f2302bc638
Filip Stromback <fstromback@gmail.com>
2013-05-07 16:01:09 +0200

    Fix a little in the tests

--- src/tests/loops_test/loops_test.vhd
+++ src/tests/loops_test/loops_test.vhd
@@ -155,7 +155,7 @@ begin
     Cpu_Allowed <= '1';
     wait until rising_edge(Clk);
     
-    for I in 1 to 600 loop
+    for I in 1 to 1200 loop
       wait until rising_edge(Clk);
     end loop; 
     
